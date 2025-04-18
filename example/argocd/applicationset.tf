fb8628a6e8190b5408d1b7738a7cb24bf4914492
Alex Christopher Ramos <alexchrisramos@Alex-Christopher-Ramos-MacBook-Pro.local>
2025-04-18 10:45:02 +0800

    feat(terraform): separate argo kinds from castai

---
+++ example/argocd/applicationset.tf
@@ -0,0 +1,42 @@
+resource "kubernetes_manifest" "castai_applicationset" {
+    manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind" = "ApplicationSet"
+    "metadata" = {
+      "name" = format("%s-%s",var.aws_cluster_name,"appset")
+      "namespace" = "argocd"
+    }
+    "spec" = {
+      "generators" = [
+        {
+        "git" = {
+          "repoURL" = var.gitops_repo_url
+          "revision" = "HEAD"
+          "directories" = [
+            {
+              "path" = "deploy/*"
+            }
+          ]
+          }
+        }
+        ]
+      "template" = {
+        "metadata" = {
+          "name" = format("%s-%s-{{path.basename}}",var.aws_cluster_name,"castai")
+          }
+        "spec" = {
+          "project" = format("%s-%s",var.aws_cluster_name,"project")
+          "source" = {
+            "repoURL" = var.gitops_repo_url
+            "path" = "{{path}}"
+            "targetRevision" = "HEAD"
+            }
+          "destination" = {
+            "server" = "https://kubernetes.default.svc"
+            "namespace" = "castai-agent"
+            }
+          }
+        }
+      }
+    }
+}
